677df3ed5ffa736d2d2f8aee0b909caaeb8f462f
”tanakatatsuya” <tanaka@gmail.com>
2020-03-07 16:30:00 +0900

    関数の読み方

---
+++ chap1_10_1.php
@@ -0,0 +1,7 @@
+<?php
+$msg = "ハロー！";
+$prise = 150;
+var_dump($msg);
+var_dump($prise);
+var_dump($msg . $prise);
+?>
---
+++ chap1_9_1.php
@@ -0,0 +1,4 @@
+<?php
+var_dump("入力せよ");
+$msg = fgets(STDIN);
+var_dump($msg);

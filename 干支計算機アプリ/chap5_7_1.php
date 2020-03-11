99b08e82bc682bef64f809c64466efe6d9f4148a
”tanakatatsuya” <tanaka@gmail.com>
2020-03-11 17:19:57 +0900

    干支計算機

---
+++ 干支計算機アプリ/chap5_7_1.php
@@ -0,0 +1,33 @@
+<?php
+//干支テェックアプリ
+//干支の計算処理を書く
+//干支テェックアプリのHTML要素を書く
+
+function h($str){
+    return htmlspecialchars($str,
+    ENT_QUOTES,"UTF-8");
+
+}
+function eto($year){
+    $etos = ["子","丑","寅","卯","辰",
+"巳","午","未","申","申","酉","戌","亥"];
+    return $etos[($year - 4)% 12];
+
+}
+$year = filter_input(INPUT_GET,"year",
+FILTER_VALIDATE_INT);
+?>
+<!DOCTYPE html>
+<title>干支計算機</title>
+<h1>干支計算機</h1>
+<?php if (empty($year)): ?>
+    <p>数字を入力してください</p>
+    <form method="get">
+    <label>年</label>:
+    <input name="year" type="number"
+    value="<?= h(date("Y")) ?>">
+    </form>
+<?php else: ?>
+    <p><?= h($year)      ?>年は</p>
+<?= eto($year)      ?>年です。</p>
+<?php endif;      ?>
